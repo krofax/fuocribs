9639ba003761df4e9cb959a59496f5511d5321c8
Blessing Krofegha <bkrofegha@gmail.com>
2018-10-27 11:50:45 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <title>Fuocribs</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet"  href="css/bootstrap.min.css">
 
 
 </head>
